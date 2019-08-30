d25d8da33ee4c3fe6ab161ae4cf073599600aaf0
Yunjeong Goo <heidiyun.goo@gmail.com>
2019-08-30 18:32:30 +0900

    BufferGeometry 기초-3(BufferGeometry-파형만들기-리팩토링)

--- src/views/example1/example1.ts
+++ src/views/example1/example1.ts
@@ -1,6 +1,17 @@
 import { Vue, Component } from 'vue-property-decorator';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { BufferAttribute } from 'three';
+
+interface SampleAttributes {
+  position: THREE.BufferAttribute;
+  color: THREE.BufferAttribute;
+}
+
+interface SampleProps {
+  positions: Float32Array;
+  color: Float32Array;
+}
 
 @Component({})
 export default class Example1 extends Vue {
@@ -11,9 +22,13 @@ export default class Example1 extends Vue {
   private scene: THREE.Scene = new THREE.Scene();
   private camera!: THREE.Camera;
   private renderer!: THREE.WebGLRenderer;
-  private cube!: THREE.Mesh;
+
+  private geometry!: THREE.BufferGeometry;
   private material!: THREE.MeshBasicMaterial;
-  private geometry: THREE.BufferGeometry = new THREE.BufferGeometry();
+  private attribute!: SampleAttributes;
+  private props!: SampleProps;
+
+  private cube!: THREE.Mesh;
   private value: number = 0;
   private control!: OrbitControls;
 
@@ -29,7 +44,7 @@ export default class Example1 extends Vue {
 
     //aspect 종횡비
     // fov (field of View) : 화각 () 확대를 하고싶으면 fov를 줄이면 된다.
-    this.camera = new THREE.PerspectiveCamera(200, width / height, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(70, width / height, 0.1, 1000);
 
     // this.camera = new THREE.OrthographicCamera(
     //   -2 * (width / height),
@@ -59,35 +74,37 @@ export default class Example1 extends Vue {
 
     this.$refs.renderer.appendChild(this.renderer.domElement);
 
-    // const geometry = new THREE.BoxGeometry(1, 1, 1);
-    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // const cube = new THREE.Mesh(geometry, material);
+    this.geometry = new THREE.BufferGeometry();
 
     const x = 20;
     const y = 20;
+    const count = (x + 1) * (y + 1);
 
-    // const vertices = new Float32Array(x * y * 18);
+    this.props = {
+      positions: new Float32Array(count * 3),
+      color: new Float32Array(count * 3)
+    };
 
-    // for (let j = 0; j < y; j++) {
-    //   for (let i = 0; i < x; i++) {
-    //     vertices.set(
-    //       [i, -j, 1, i + 1, -j, 1, i + 1, 1, 1, i + 1, 1, 1, i, 1, 1, i, -j, 1],
-    //       (i + j * x) * 18
-    //     );
-    //   }
-    // }
-
-    const vertices = new Float32Array((x + 1) * (y + 1) * 3);
-    const colors = new Float32Array((x + 1) * (y + 1) * 3);
+    this.attribute = {
+      position: new THREE.BufferAttribute(this.props.positions, 3),
+      color: new THREE.BufferAttribute(this.props.color, 3)
+    };
 
     for (let j = 0; j < y + 1; j++) {
       for (let i = 0; i < x + 1; i++) {
-        vertices.set(
+        this.props.positions.set(
           [i - (x - x / 2), j - (y - y / 2), 1],
           (i + j * (x + 1)) * 3
         );
+
+        const r = (j * (i / x)) / 255;
+        const g = (j * (i % x)) / 255;
+
+        this.props.color.set([r, g, 1], (i + j * (x + 1)) * 3);
       }
     }
+
+    console.log(this.props.positions);
 
     const indices: number[] = [];
 
@@ -103,13 +120,12 @@ export default class Example1 extends Vue {
 
     this.geometry.setIndex(indices);
 
-    this.geometry.addAttribute(
-      'position',
-      new THREE.BufferAttribute(new Float32Array(vertices), 3)
-    );
+    this.geometry.addAttribute('position', this.attribute.position);
+    this.geometry.addAttribute('color', this.attribute.color);
 
     this.material = new THREE.MeshBasicMaterial({
-      side: THREE.DoubleSide
+      side: THREE.DoubleSide,
+      vertexColors: THREE.VertexColors
     });
     this.cube = new THREE.Mesh(this.geometry, this.material);
 
@@ -128,24 +144,36 @@ export default class Example1 extends Vue {
     // this는 class가 주인인데, 시스템에서 호출할 때는 this가 바껴서 들어와서 this.render를 찾을 수 없는 것.
 
     this.control.update();
-    this.value += 0.2;
+    this.value += 0.03;
 
-    let t = 0;
+    // for (let j = 0; j < 21; j++) {
+    //   let a = this.value;
+    //   for (let i = 0; i < 21; i++) {
+    //     a += 0.03;
+    //     this.props.positions[i + j * 21 * 21 + 2 * (i + 1)] =
+    //       Math.sin(a + i + j * 21 * 0.003) * 0.7;
+    //   }
+    // }
+
     for (
       let i = 0;
       i < this.geometry.getAttribute('position').array.length;
       i++
     ) {
-      t += 0.2;
+      this.geometry
+        .getAttribute('position')
+        .setZ(i, Math.sin(this.value + i * 0.01) * 0.9);
       // 값이 커질수록 물결이 많아짐..왜?
       if (i % 3 === 2) {
-        this.geometry.getAttribute('position').array[i] +=
-          Math.sin(this.value + t) * 0.02;
+        // this.geometry.getAttribute('position').array[i] +=
+        // Math.sin(this.value + i * 0.003) * 0.07;
+        
         // 값이 커질수록 범위가 커져서 폭이 커지는 것인가?
       }
     }
 
     this.geometry.getAttribute('position').needsUpdate = true;
+
     this.renderer.render(this.scene, this.camera);
   }
 }
